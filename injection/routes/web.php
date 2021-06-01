ef3bcbbd607193b67898e283b0dfe71a13ea6bf9
quanle031198 <bluestargaming000@gmail.com>
2021-06-01 11:35:28 +0700

    injection

---
+++ injection/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function (\Illuminate\Http\Request $request) {
+  if (!empty($request->value)) {
+      $customers = DB::table('customer')->where('id', $request->value)->get();
+  }
+  dump('SELECT * FROM customer WHERE id = ' . $request->value);
+  dd($customers);
+  return view('welcome');
+});
